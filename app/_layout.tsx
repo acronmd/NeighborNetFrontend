05c37094395bbcc4c6c0632238ae11ae77275f7b
acronmd <aidanadame20@gmail.com>
2025-12-03 14:45:28 -0500

    Fix React imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { PostProvider } from "@/app/data/demoPostData"; // ← create this file below
 import { EventProvider } from "@/app/data/demoEventData";
+import React from "react";
 
 export default function RootLayout() {
     return (
