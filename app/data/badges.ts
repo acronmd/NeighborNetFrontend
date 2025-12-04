ef3cc2deac46085f0a05ce50c6b0947bbfec5785
alawiaadill <alawiaadil79@gmail.com>
2025-12-04 00:22:20 -0500

    Add badges module with image imports

---
+++ app/data/badges.ts
@@ -0,0 +1,10 @@
+
+const badges = {
+        incidentReporter: require("@/assets/images/badges/incident.png"),
+        verifiedReporter: require("@/assets/images/badges/verified.png"),
+        communityHelper: require("@/assets/images/badges/community.png"),
+        eventParticipant: require("@/assets/images/badges/event.png"),
+      };
+      
+      export default badges;
+      export { badges };
